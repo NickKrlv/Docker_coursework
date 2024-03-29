221ac06d402601e38006422509d8602329c9a6a1
Nick_PC <fieruo@gmail.com>
2024-03-29 20:58:33 +0300

    README

--- users/services.py
+++ users/services.py
@@ -5,7 +5,7 @@ from config.settings import TELEGRAM_BOT_TOKEN
 class MyBot:
     URL = "https://api.telegram.org/bot"
     TOKEN = TELEGRAM_BOT_TOKEN
-    chat_id = '359964166'
+    chat_id = ''  # введи свой chat_id
 
     def send_message(self, text):
         requests.get(f"{self.URL}{self.TOKEN}/sendMessage?chat_id={self.chat_id}&text={text}")
